95961d9c239e7489efbef2087a52f92a4bdf9ea4
aaa1999 <2224525664@qq.com>
2020-10-15 20:12:29 +0800

    Second Commit

--- translateAll3.py
+++ translateAll3.py
@@ -398,6 +398,7 @@ def evaluateRandomly(encoder, decoder, n=10):
         print('')
 
 hidden_size = 256
+
 encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
 attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
 
